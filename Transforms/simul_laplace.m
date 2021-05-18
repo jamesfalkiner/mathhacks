170ea38a8680f499697da1cc8ff4c25dc634f8e5
jamesfalkiner <68999748+jamesfalkiner@users.noreply.github.com>
2021-05-18 15:31:45 +0100

    finally

--- Transforms/simul_laplace.m
+++ Transforms/simul_laplace.m
@@ -21,16 +21,15 @@ leqn1= laplace(eqn1,t,s)
 leqn2=laplace(eqn2,t,s)
 %inpuit initial conditions
 LT_Y=subs(leqn1,laplace(x,t,s),X);
-LT_Y=subs(leqn1,laplace(y,t,s),Y);
+LT_Y=subs(LT_Y,laplace(y,t,s),Y);
 LT_X=subs(leqn2,laplace(x,t,s),X);
-LT_X=subs(leqn2,laplace(y,t,s),Y);
+LT_X=subs(LT_X,laplace(y,t,s),Y);
 x0= input('x(0) equals what?\n');
 y0= input('y(0) equals what?\n');
 LT_Y=subs(LT_Y, x(0), x0);                  
 LT_Y=subs(LT_Y, y(0), y0);
-LT_X=subs(LT_Y, x(0), x0);                  
-LT_X=subs(LT_Y, y(0), y0);
+LT_X=subs(LT_X, x(0), x0);                  
+LT_X=subs(LT_X, y(0), y0);
 answer=solve([LT_Y,LT_X],[Y X]);
 x = ilaplace(answer.X,s,t)
 y = ilaplace(answer.Y,s,t)
-ezplot(x,y); shg
