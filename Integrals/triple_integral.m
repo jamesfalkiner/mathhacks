58c44d5fdd18a6c143e43496a7a2627f1fe9b21d
HarryHD123 <pn19345@bristol.ac.uk>
2021-05-23 15:43:07 +0100

    updates

--- Integrals/triple_integral.m
+++ Integrals/triple_integral.m
@@ -5,16 +5,16 @@ limit2 =input('insert limit 2(inner limit bottom)\n');
 firstintegral =input('integrate 1st in terms of?(inner)\n');
 limit3 =input('insert limit 3(middle limit top)\n');
 limit4 =input('insert limit 4(middle limit bottom)\n');
-secondintegral = input('integrate 2nd in terms of?(outer)\n');
-limit5 =input('insert limit 5(middle limit top)\n');
-limit6 =input('insert limit 6(middle limit bottom)\n');
+secondintegral = input('integrate 2nd in terms of?(middle)\n');
+limit5 =input('insert limit 5(outer limit top)\n');
+limit6 =input('insert limit 6(outer limit bottom)\n');
 thirdintegral = input('integrate 3nd in terms of?(outer)\n');
 
 intone =  int(input1, firstintegral, limit1, limit2)
 inttwo = int(intone, secondintegral, limit3, limit4)
 intthree = int(inttwo, thirdintegral, limit5, limit6)
 
-
+%May be negative, manually check with second integral
 %note to find second moment of area you need to evaluate int(y.^2)dxdy first sketch, then you do the
 %limits as the curves in terms of y and integrate with x then you do the y
 %limits and integrate with with y then do it all again but with int(x.^2)dxdy
